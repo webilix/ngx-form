81840b216e38f826a7980c134d467d733249499d
Ali Amirnezhad <webilix@gmail.com>
2023-10-19 00:19:06 +0330

    Update AutoFocusDirective to use AfterViewInit

--- ngx-form/src/lib/ngx-directive/ngx-autofocus.directive.ts
+++ ngx-form/src/lib/ngx-directive/ngx-autofocus.directive.ts
@@ -1,18 +1,20 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({ selector: '[ngxAutoFocus]' })
-export class NgxAutoFocusDirective implements OnInit {
+export class NgxAutoFocusDirective implements AfterViewInit {
     @Input({ required: true }) public ngxAutoFocus!: boolean;
 
     constructor(private elementRef: ElementRef) {}
 
-    ngOnInit(): void {
+    ngAfterViewInit(): void {
         if (!this.ngxAutoFocus || !this.elementRef) return;
 
         const input: HTMLInputElement = this.elementRef.nativeElement as HTMLInputElement;
         if (!input) return;
 
-        if (input.focus) input.focus();
-        if (input.select) input.select();
+        setTimeout(() => {
+            if (input.focus) input.focus();
+            if (input.select) input.select();
+        }, 0);
     }
 }
